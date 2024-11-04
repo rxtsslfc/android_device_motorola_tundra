0ab24cff7ccf2e4d6281d695659b4babf0b57a1e
rxtsslfc <58660132+rxtsslfc@users.noreply.github.com>
2024-11-04 13:28:16 +0800

    更新 vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_tundra-user
-add_lunch_combo omni_tundra-userdebug
-add_lunch_combo omni_tundra-eng
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_tundra.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_tundra-user \
+    omni_tundra-userdebug \
+    omni_tundra-eng
