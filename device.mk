8b0975d79269d0298df872b46da667181d5156d4
rxtsslfc <58660132+rxtsslfc@users.noreply.github.com>
2024-11-04 19:29:48 +0800

    更新 device.mk

--- device.mk
+++ device.mk
@@ -21,11 +21,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.lahaina
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.lahaina \
-    libgptutils \
-    libz \
-    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
