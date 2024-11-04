4599e294ac1ff231210a25e5a87f7cc6e2c3d9cd
rxtsslfc <58660132+rxtsslfc@users.noreply.github.com>
2024-11-04 20:52:25 +0800

    更新 omni_tundra.mk

--- omni_tundra.mk
+++ omni_tundra.mk
@@ -9,6 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from tundra device
 $(call inherit-product, device/motorola/tundra/device.mk)
